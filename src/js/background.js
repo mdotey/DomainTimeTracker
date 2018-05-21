9b7baff7e5d6a01fb6679efd937f6a4cf1300144
Mathew Dotey <mathewdotey@gmail.com>
2018-05-21 13:43:27 -0700

    Implemented tab switch event listener

--- src/js/background.js
+++ src/js/background.js
@@ -12,7 +12,7 @@ chrome.tabs.query({'active' : true, 'currentWindow': true}, function(tabs){
 	currentTimer = Date.now();	
 });
 
-//Change the timer to new website when switching pages
+//Detect when goign to a new domain and change timers accordingly
 chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab){
 	chrome.tabs.query({'active' : true, 'currentWindow': true}, function(tabs){
 		let url = new URL(tabs[0].url);
@@ -34,6 +34,27 @@ chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab){
 	});
 });
 
+//Detect when tab is switched and switch timers accordingly
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	chrome.tabs.query({'active' : true, 'currentWindow': true}, function(tabs){
+		let url = new URL(tabs[0].url);
+		//Check if user has gone to a new domain and not just a new url within the same domain
+		if (currentDomain != url.hostname) {
+			
+			//Check if old domain has been visited before
+			if (currentDomain in domainTimeDict) {
+				domainTimeDict[currentDomain] = Date.now() - currentTimer + domainTimeDict[currentDomain];
+				
+			}
+			else {
+				domainTimeDict[currentDomain] = Date.now() - currentTimer;
+			}
+			//update current domain
+			currentDomain = url.hostname;
+			currentTimer = Date.now();
+		}
+	});
+});
 
 //Send the total time for website request
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
